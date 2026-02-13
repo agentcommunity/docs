46e808fb4608a68dc558681f240d190be6b18ed8
nembal <nembal@gmail.com>
2026-02-13 08:16:14 -0800

    middleware -> proxy as per latest

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const BLOG_HOST = 'blog.agentcommunity.org';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const hostname = request.headers.get('host') || '';
   const { pathname } = request.nextUrl;
 
